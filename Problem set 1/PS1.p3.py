51435edc26e253113912e597957db01bb843702a
tlash2003 <dg62810@mail.harpercollege.edu>
2025-01-21 17:51:54 -0600

    Add files via upload
    Ps1.p3

---
+++ Problem set 1/PS1.p3.py
@@ -0,0 +1,8 @@
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+circumference = 2 * (length + width)
+
+print("The area of the rectangle is " + str(area))
+print("The circumference of the rectangle is " + str(circumference))
