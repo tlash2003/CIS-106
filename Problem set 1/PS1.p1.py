fa50170517e6bd728d5e6a701c6c464189fda525
tlash2003 <dg62810@mail.harpercollege.edu>
2025-01-21 18:34:01 -0600

    Add files via upload
    ps1.1

---
+++ Problem set 1/PS1.p1.py
@@ -0,0 +1,6 @@
+quantity = float(input("Enter the quantity: "))
+price_per_unit = float(input("Enter the price per unit: "))
+
+ext_price = quantity * price_per_unit
+
+print(f"Extended price: {ext_price}")
