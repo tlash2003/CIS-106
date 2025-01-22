5bb21e3f7019e82b69eb91ce1664d1b9530ce83d
tlash2003 <dg62810@mail.harpercollege.edu>
2025-01-21 18:34:51 -0600

    Add files via upload

---
+++ Problem set 1/PS1.p4.py
@@ -0,0 +1,8 @@
+last_name = input("Enter your last name: ")
+credits_taken = float(input("Enter the number of credits you have taken: "))
+
+lab_fee= 100
+total_tuition = credits_taken * 250 + lab_fee
+
+print (f"Last Name: {last_name}")
+print (f"Total Tuition: {total_tuition}")
