534269af29c9d178eb4d45bc0ba4ae39dca132c2
tlash2003 <dg62810@mail.harpercollege.edu>
2025-01-21 18:35:20 -0600

    Delete Problem set 1/ps1.p2.py

--- Problem set 1/ps1.p2.py
+++
@@ -1,8 +0,0 @@
-last_name = str(input("Enter last name"))
-hours= int(input("Enter hours worked"))
-pay_rate= float(input("Enter pay rate per hour"))
-
-gross_pay= hours * pay_rate
-
-print("Last name: ", last_name)
-print("Gross pay: ", gross_pay)
